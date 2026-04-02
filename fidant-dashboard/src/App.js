e77fd533e35b1a8ed328771440b53dac127e5dc0
elsatobin <elsa0010021@outlook.com>
2026-04-02 22:18:48 +0300

    complete first step

---
+++ fidant-dashboard/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import UsageChart from "./UsageChart";
+
+function App() {
+    return (
+        <div className="App">
+            <UsageChart />
+        </div>
+    );
+}
+
+export default App;
--- fidant_analytics/python-service/main.py
+++ fidant_analytics/python-service/main.py
@@ -6,8 +6,16 @@ from datetime import datetime, timedelta
 from db import Base, engine, get_db
 import models
 from auth import decode_token, create_access_token
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # React dev 서버 주소
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 Base.metadata.create_all(bind=engine)
 
